a40a55e42fe7a9b4f53a607b688bb7a44cbad65d
kean ocampo <ocampokeankenrick_bsit@plmun.edu.ph>
2025-03-05 08:41:29 +0800

    Add files via upload

---
+++ python problem_02.py
@@ -0,0 +1,8 @@
+# Kean Ken Rick Ocampo
+# ITELEC2
+# Problem Set 01 - Problem 01
+# Simple Calculator Program
+
+print("Square the Number Program")
+number = int(input("Enter a number: "))
+print(f"The square of {number} is {(number * number):.2f}")
